c27e71ceb20fbee00d36354054d670a8b13b53a6
mlincicum <mglincicum@gmail.com>
2025-10-14 09:38:56 -0400

    debug tile selection indicator

--- Assets/Scripts/Battle States/MoveSequenceState.cs
+++ Assets/Scripts/Battle States/MoveSequenceState.cs
@@ -2,7 +2,7 @@ using System.Collections;
 /**
  * Author: Megan Lincicum
  * Date Created: 10/01/25
- * Date Last Updated: 10/01/25
+ * Date Last Updated: 10/14/25
  * Summary: A state to allow the current move to animate uninterupted
  */
 public class MoveSequenceState : BattleState
@@ -15,8 +15,10 @@ public class MoveSequenceState : BattleState
 
     IEnumerator Sequence()
     {
+        tileManager.DeselectTile();
         Movement movement = owner.CurrentUnit.GetComponent<Movement>();
         yield return StartCoroutine(movement.Traverse(owner.currentTile));
+        tileManager.SelectTile(owner.currentTile);
         owner.ChangeState<SelectActionState>();
     }
 }
